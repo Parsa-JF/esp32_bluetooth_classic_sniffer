3521e3849be4ec7406e55f20258c24ac4faa418a
parsa-jf <parsafares@gmail.com>
2023-05-24 21:31:00 -0700

    Add Mayhem

--- host_stack/bluekitchen/test/fuzz/fuzz_ad_parser.c
+++ host_stack/bluekitchen/test/fuzz/fuzz_ad_parser.c
@@ -3,9 +3,11 @@
 
 #include "ad_parser.h"
 
-int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
     // ad parser uses uint88_t length
-    if (size > 255) return 0;
+    if (size > 255)
+        return 0;
     // test ad iterator by calling simple function that uses it
     ad_data_contains_uuid16(size, data, 0xffff);
     return 0;
